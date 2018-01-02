41f9b2fb31f3204c252c4b319c28d44c6b480e9e
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-02 03:52:51 +0000

    Solved algorithm and tested

--- Day-1.py/IsUnique.py
+++ Day-1.py/IsUnique.py
@@ -6,3 +6,24 @@
 ###############################################################################################################################
 
 
+def isUnique(str1, str2):
+    dict1 = {}
+    dict2 = {}
+
+    for item in str1:
+        dict1[item] = dict1.get(item, 0) + 1
+
+    for item in str2:
+        dict2[item] = dict2.get(item, 0) + 1
+
+
+    if dict1 == dict2:
+        return True
+    else:
+        return False
+
+
+
+isUnique("santa", "satan")
+
+isUnique("fries", "friends")
