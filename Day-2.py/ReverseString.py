1b103214aebba6cbe9e9689b7eb710b3012edabf
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-03 00:35:15 +0000

    added file and solution

---
+++ Day-2.py/ReverseString.py
@@ -0,0 +1,14 @@
+# Day 2/365 #AnAlgorithmAday2018
+# Problem is from Leetcode 344
+# Reverse String
+#
+# Write a function that takes a string as input and returns the string reversed.
+#
+#Example:
+#Given s = "hello", return "olleh".
+###############################################################################################################################
+
+def reverseString(str):
+    return s[::-1]
+
+    
