00159f75772138eb179f7d25dd83da850216eb19
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-10 22:25:02 +0000

    wrote algorithm and solved

--- Day-10/singleNumber.py
+++ Day-10/singleNumber.py
@@ -8,3 +8,18 @@
 #Note:
 #Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 ########################################################################################################################
+
+def singleNumber(lst):
+
+    dict_values ={}
+
+    for item in lst:
+        dict_values[item] = dict_values.get(item, 0) + 1
+
+
+    for key, value in dict_values.iteritems():
+        if value == 1:
+            return key
+
+
+    return 0
