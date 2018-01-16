718283fb9f484948b30a1bab02066a2f4bdca05c
Taraneh Tayaran <ttayaran@gmail.com>
2018-01-16 01:10:29 +0000

    added algorithm and solution

---
+++ Day-15/mergeTwoSortedLists.py
@@ -0,0 +1,29 @@
+# Day 15/365 #AnAlgorithmAday2018
+# Problem is from Leetcode 21
+#
+# Merge Two Sorted Lists
+#
+#Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+#
+#Example:
+#
+#Input: 1->2->4, 1->3->4
+#Output: 1->1->2->3->4->4
+########################################################################################################################
+
+class ListNodes(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def mergeTwoSortedLists(self, l1, l2):
+
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+
+            l1.next = self.mergeTwoSortedLists(l1.next, l2)
+
+        return l1 or l2
